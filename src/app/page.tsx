63062a8f3a478765c3773e08e7c266b1c4823f8f
jerry <jerry@acryl.ai>
2025-01-20 16:51:30 +0900

    [feat] 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Header } from "@/components/Header";
+import { Main } from "next/document";
+
 export default function Home() {
   return (
     <div>
-      Hellow Nex
+      <Header />
+      <Main />
     </div>
   );
 }
