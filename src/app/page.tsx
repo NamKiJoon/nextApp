592fc2d4799991a965f9540e4bad4f5e3aab0551
jerry <jerry@acryl.ai>
2025-01-24 14:31:56 +0900

    main 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
-import { Inter, Noto_Sans_KR } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-const notoSans = Noto_Sans_KR({ subsets: ["latin"] });
 export default function Home() {
   return (
-    <main className={notoSans.className}>
+    <main>
       <header>
         <Header />
       </header>
